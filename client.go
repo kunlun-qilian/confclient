6e18028b23442a8ff4dffa431fdee2ffec663c54
pippozq <zengqi0529@163.com>
2023-07-06 12:48:07 +0800

    update

--- client.go
+++ client.go
@@ -3,28 +3,35 @@ package confclient
 import (
 	"context"
 	"fmt"
+	"net"
 	"net/http"
+	"time"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
 )
 
-type RestClient struct {
+type Client struct {
 	// IP or domain
 	Host string `env:""`
 	// Port
 	Port int `env:""`
 	// HTTP HTTPS
 	Protocol string `env:""`
-
-	host string
+	// Second
+	Timeout time.Duration
+	client  *http.Client
 }
 
-func (c *RestClient) ApiServer() string {
-	return c.host
+func (c *Client) ApiServer() string {
+	return fmt.Sprintf("%s://%s:%d", c.Protocol, c.Host, c.Port)
 }
 
-func (c *RestClient) SetDefaults() {
+func (c *Client) SetDefaults() {
+	if c.Host == "" {
+		c.Host = "127.0.0.1"
+	}
+
 	if c.Protocol == "" {
 		c.Protocol = "http"
 	}
@@ -33,11 +40,35 @@ func (c *RestClient) SetDefaults() {
 		c.Port = 80
 	}
 
-	c.host = fmt.Sprintf("%s://%s:%d", c.Protocol, c.Host, c.Port)
+	if c.Timeout == 0 {
+		c.Timeout = 5
+	}
 }
 
-func (c *RestClient) Init() {
+func (c *Client) Init() {
 	c.SetDefaults()
+	c.client = &http.Client{
+		Timeout: c.Timeout * time.Second,
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		},
+	}
+}
+
+func (c *Client) Do(req *http.Request) (*http.Response, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
 // WithTrace Inject trace
--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module github.com/kunlun-qilian/confclient
 
-go 1.17
+go 1.19
 
 require go.opentelemetry.io/otel v1.9.0
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	go.opentelemetry.io/otel/trace v1.9.0 // indirect
